c73d96232ad82a146c7cf8b23a4b9332ea6e6829
MATIASDELGADO10 <muhammedcimen6327@gmail.com>
2026-06-04 00:59:52 +0300

    Add files via upload

---
+++ learn.py
@@ -0,0 +1,23 @@
+"""import random
+
+sayi = random.randint(1, 20)
+i = 3
+
+while i > 0:
+    tahmin = input("Sayı tahmin oyununa hoş geldiniz. 1 ile 20 arasında bir tamsayı giriniz: ")
+
+    try:
+        tahmin = int(tahmin)
+    except ValueError:
+        print("Lütfen bir tamsayı giriniz!!!")
+        continue
+
+    if tahmin == sayi:
+        print("Doğru tahmin, tebrikler!!")
+        break
+    else:
+        i -= 1
+        print(f"Yanlış tahmin, kalan tahmin hakkınız: {i}")
+
+        if i == 0:
+            print(f"Doğru sayı: {sayi}")"""
